7f7f3dbd8b95c47299d966adcea1665bcda960c3
ducthangvu <vuducthang91@gmail.com>
2025-06-19 23:15:05 +0200

    fix(radio-group): fix radio group component

--- src/app/_game/ui/radio-group.tsx
+++ src/app/_game/ui/radio-group.tsx
@@ -1,4 +1,5 @@
 import styles from '@/app/_game/ui/radio-group.module.css';
+import { useState } from 'react';
 
 interface Props {
     legend: string;
@@ -7,13 +8,23 @@ interface Props {
 }
 
 export default function RadioGroup({ legend, name, options}: Props) {
+    const [value, setValue] = useState<typeof options[number] | null>(null);
+
     return (
         <fieldset className={styles.formControl}>
             <legend className={styles.formControl__legend}>{legend}</legend>
 
             { options.map((o: string, i: number) => (
                 <label className={styles.formControlRadio} key={i}>
-                    <input type="radio" id={'n' + i} value={o} name={name} required/>
+                    <input
+                        type="radio"
+                        id={'n' + i}
+                        value={o}
+                        name={name}
+                        required
+                        checked={value === o}
+                        onChange={() => setValue(o)}
+                    />
                     <span>{o}</span>
                 </label>
             ))}
