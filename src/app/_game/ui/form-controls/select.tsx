7f1ec058804e289942b9e24c36d2249fc99dbd24
ducthangvu <vuducthang91@gmail.com>
2025-07-13 19:05:54 +0200

    refactor(frontend): improve select component, improve select in app

--- src/app/_game/ui/form-controls/select.tsx
+++ src/app/_game/ui/form-controls/select.tsx
@@ -1,30 +1,26 @@
 import { useState } from 'react';
 import styles from '@/app/_game/ui/form-controls/select.module.css';
-import { Option, optionAdapter } from '@/app/_game/ui/form-controls/option';
+import { FormField } from '@/app/_game/ui/form-controls/form-field';
 
-interface Props<T extends string> {
+interface Props {
+    children: React.ReactNode;
     legend: string;
     name: string;
-    options: (T | Option<T>)[];
 }
 
-export default function Select<T extends string>({ legend, name, options}: Props<T>) {
+export default function Select<T extends string>({ children, legend, name }: Props) {
     const [value, setValue] = useState<T | ''>('');
-    const optionsData: Option<T>[]  = options.map(optionAdapter);
 
     return (
-        <fieldset className={styles.formControl}>
-            <legend className={styles.formControl__legend}>{legend}</legend>
+        <FormField legend={legend}>
             <select
                 className={styles.formControlSelect}
                 name={name}
                 value={value ?? undefined}
                 onChange={(v) => setValue(v.target.value as T ?? '')}
             >
-            { optionsData.map((o: Option<T>) => (
-                <option value={o.value} key={o.value}>{o.label}</option>
-            ))}
+                { children }
             </select>
-        </fieldset>
+        </FormField>
     )
 }
